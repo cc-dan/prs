427f71658fd9fff28633ccb4bf820f93f22d3702
BrunoLisanti <brunodavidlisanti@gmail.com>
2025-08-28 20:53:34 +0000

    Agregado poder clickear perros

---
+++ scripts/world.gd
@@ -0,0 +1,25 @@
+extends Node3D
+@onready var camera: Node3D = $camera/Base/Camera3D
+
+signal dog_clicked
+
+func cast_ray(mask: int) -> Dictionary:
+	const RAY_LENGTH = 1000.0
+	var space_state = get_world_3d().direct_space_state
+	var from = camera.project_ray_origin(get_viewport().get_mouse_position())
+	var to = from + camera.project_ray_normal(get_viewport().get_mouse_position()) * RAY_LENGTH
+	#El tercer parametro es para setear la mascara de collision, y esta es un 
+	#entero para poder cumplir con todas las combinaciones de la mascara de la 1 a la 20
+	# Si se quiere que solo este en la capa n, la mascara debe estar en 1 << (n - 1), por lo tanto
+	# 1 << 1 hace que solo este en la capa 2. 
+	var query = PhysicsRayQueryParameters3D.create(from, to, 1 << mask - 1) 
+	query.set_collide_with_areas(true)
+	var result = space_state.intersect_ray(query)
+	return result
+
+func _process(delta: float) -> void:
+	var result: Dictionary = cast_ray(2)
+	if Input.is_action_just_pressed("lclick"):
+		if result.size() > 0:
+			dog_clicked.emit()
+			print(result["collider"])
