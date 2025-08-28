42e188adadea8e1c6a650e5a8276e954d10ce471
dante <dantecocciolo@gmail.com>
2025-08-28 18:11:31 -0300

    Título de las ventanas centrado correctamente, escena de ícono de escritorio

--- scripts/missing_dog_window.gd
+++ scripts/missing_dog_window.gd
@@ -3,7 +3,7 @@ extends "res://scripts/window.gd"
 var pet_structure = preload("res://scripts/pet_structure.gd")
 
 func init(pet_info: Dictionary):
-	%Title.text += pet_info["id"]
+	%Title.text = "Missing: " + pet_info["id"]
 	%SpotsInfo.text = pet_structure.describe(pet_info["hair_spots"])
 	%EarsInfo.text = pet_structure.describe(pet_info["ears"])
 	%NoseInfo.text = pet_structure.describe(pet_info["nose"])
