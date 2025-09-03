00ea7af9b19838bfc04ca218b15eb9623623682c
dante <dantecocciolo@gmail.com>
2025-09-03 19:36:55 -0300

    Animación de los perros, movimiento de los perros más seguido, arreglado que todos los perros empiecen a moverse al unísono

--- scripts/dog.gd
+++ scripts/dog.gd
@@ -1,6 +1,7 @@
 extends StaticBody3D
 
 @export var id: String
+@onready var animation_player: AnimationPlayer = find_child("AnimationPlayer")
 
 @export var navigation: NavigationRegion3D
 var map: RID
@@ -10,8 +11,9 @@ var path: PackedVector3Array
 var path_index: int = 0
 
 func _ready() -> void:
-	if navigation:
-		map = navigation.get_navigation_map()
+	map = navigation.get_navigation_map()
+	$Idler.wait_time = randf_range(0, 2)
+	$Idler.start()
 
 func random_point_in_circle(radius: float) -> Vector2:
 	var x := randf_range(-radius, radius)
@@ -52,4 +54,9 @@ func _on_idler_timeout() -> void:
 	var point := Vector2(global_position.x, global_position.z) + random_point_in_circle(randf_range(2, 8))
 	set_path(NavigationServer3D.map_get_closest_point(map, Vector3(point.x, global_position.y, point.y)))
 	idle = false
-	$Idler.wait_time = randf_range(5, 10)
+	animation_player.play("Animation")
+	$Idler.wait_time = randf_range(2, 5)
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	animation_player.stop() if idle else animation_player.play()
